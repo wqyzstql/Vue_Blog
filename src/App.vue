cdc43f7e2a652e4c8c0255432974db3a61261fd9
Mizukiqwq <1139198820@qq.com>
2024-08-29 11:05:07 +0800

    header and avator

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <Headbar/>
+      </el-header>
+
+      <el-container class="custom-container">
+        <el-aside width='15%'>
+          <Avator/>
+        </el-aside>
+        <el-container class="main-container">
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Headbar from './headbar.vue'
+import Avator from './avator.vue';
+
+export default {
+  components:{
+    Headbar,
+    Avator
+  }
+}
+</script>
+
+<style>
+.custom-container {
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/avator.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-row class="demo-basic">
+      <el-col>
+          <div class="block">
+            <el-avatar :size="100" :src="circleUrl" />
+          </div>
+          <div class="sub-title">MyAngelMizuki_</div>
+      </el-col>
+    </el-row>
+  </template>
+  <script lang="ts" setup>
+  import { reactive, toRefs } from 'vue'
+  
+  const state = reactive({
+    circleUrl:
+      'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+  })
+  const { circleUrl } = toRefs(state)
+  </script>
+  
+  <style scoped>
+  .demo-basic {
+    text-align: center;
+  }
+  .demo-basic .sub-title { 
+    margin-bottom: 10px;
+    font-size: 14px;
+    color: var(--el-text-color-secondary);
+  }
+  .demo-basic .demo-basic--circle {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .demo-basic .block:not(:last-child) {
+    border-right: 1px solid var(--el-border-color);
+  }
+  .demo-basic .el-col:not(:last-child) {
+    border-right: 1px solid var(--el-border-color);
+  }
+  </style>
+  
